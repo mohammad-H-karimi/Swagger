47fd0a3f6263b7b034e163c09b468998b077fc10
Reza82 <127042809+Rngpoor@users.noreply.github.com>
2025-11-14 14:45:56 +0330

    Add files via upload

---
+++ WebApplication-UN/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+namespace WebApplication_UN.Models
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ WebApplication-UN/Models/UserCredential.cs
@@ -0,0 +1,8 @@
+namespace WebApplication_UN.Models
+{
+    public class UserCredential
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
