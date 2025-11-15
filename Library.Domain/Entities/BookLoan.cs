17add6bbd51ea68b0df322b5f8977045904f7844
mohammad-H-karimi <127038460+mohammad-H-karimi@users.noreply.github.com>
2025-11-15 11:57:20 +0330

    add change .

---
+++ Library.Domain/Entities/BookLoan.cs
@@ -0,0 +1,14 @@
+ using System;
+ namespace Library.Domain.Entities
+ {
+ public class BookLoan
+ {
+ public int BookLoanId { get; set; }
+ public int MemberId { get; set; }
+ public int BookId { get; set; }
+ public DateTime BorrowDate { get; set; }
+ public DateTime? ReturnDate { get; set; }
+ public Member Member { get; set; }
+ public Book Book { get; set; }
+ }
+ }
